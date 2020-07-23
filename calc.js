bc1a72ccf36ebbd63e092a51b4c7fe347ca95c5e
j3r3s <jerito97@hotmail.com.ar>
2020-07-23 13:02:43 -0300

    lista para calculos de primaria

--- calc.js
+++ calc.js
@@ -1,1 +1,16 @@
-const leerResultado = document.querySelectorAll("#resultado");
+const leerResultado = document.querySelector("#resultado");
+
+
+function set(e) {
+
+    leerResultado.textContent += e
+
+}
+
+function result() {
+
+    resultado = eval(leerResultado.textContent);
+
+    leerResultado.textContent = resultado;
+
+}
