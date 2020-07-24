ebc309730035c7f2d6448c7528bce777f7d6d160
j3r3s <jerito97@hotmail.com.ar>
2020-07-24 14:24:23 -0300

    keyboard events,

--- calc.js
+++ calc.js
@@ -1,34 +1,48 @@
-
 const leerResultado = document.querySelector("#resultado");
 
 
 
+document.addEventListener('keydown', (event) => {
+   
+    let keyName = event.keyCode;
+    console.log(keyName)
+   
+    switch (keyName) {
+        case 13:
+            result()
+            break;
+
+        case 46:
+            C()
+            break;
+    }
+})
+
 
 function C() {
 
-   leerResultado.value = "";
-   leerResultado.style.fontSize = "1.1em";
-   leerResultado.style.color = "black";
+    leerResultado.value = "";
+    leerResultado.style.fontSize = "1.1em";
+    leerResultado.style.color = "black";
 }
 
 function set(e) {
 
-        leerResultado.value += e; 
-  
-}
+    leerResultado.value += e;
 
+}
 
 function result() {
     try {
         resultado = eval(leerResultado.value);
-    
+
         leerResultado.value = resultado;
 
     } catch (error) {
+
         leerResultado.value = "mandaste cualquiera";
         leerResultado.style.color = 'red';
         leerResultado.style.fontSize = '0.7em'
     }
-    
-    
+
 }
