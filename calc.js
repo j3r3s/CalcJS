f7e1d4c4fdd99484f1eef2e5d3b3b81eba1ec724
j3r3s <jerito97@hotmail.com.ar>
2020-07-23 21:35:19 -0300

    mensaje de error, clear, input mas limpio

--- calc.js
+++ calc.js
@@ -1,22 +1,34 @@
+
 const leerResultado = document.querySelector("#resultado");
 
 
-function clear() {
 
-    leerResultado.value = y
+
+function C() {
+
+   leerResultado.value = "";
+   leerResultado.style.fontSize = "1.1em";
+   leerResultado.style.color = "black";
 }
-
 
 function set(e) {
 
-    leerResultado.value += e
-
+        leerResultado.value += e; 
+  
 }
 
+
 function result() {
+    try {
+        resultado = eval(leerResultado.value);
+    
+        leerResultado.value = resultado;
 
-    resultado = eval(leerResultado.value);
-
-    leerResultado.value = resultado;
-
+    } catch (error) {
+        leerResultado.value = "mandaste cualquiera";
+        leerResultado.style.color = 'red';
+        leerResultado.style.fontSize = '0.7em'
+    }
+    
+    
 }
